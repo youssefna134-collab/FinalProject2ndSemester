8cd286902038630c1a655ef2f496dfc834caadb1
youss <youssefna134@gmail.com>
2026-05-14 15:15:42 -0400

    Admin class w/ all methods

--- src/main/java/org/youssefn/domain/Admin.java
+++ src/main/java/org/youssefn/domain/Admin.java
@@ -1,5 +1,22 @@
 package org.youssefn.domain;
 
-public class Admin extends User {
+import org.youssefn.Interfaces.Reportable;
 
+public class Admin extends User implements Reportable {
+
+    public Admin(String name) {
+        super(name);
+    }
+
+    @Override
+    public int getBorrowLimit() {
+        return 0;
+    }
+
+    @Override
+    public void generateReport(Library library) {
+        for (Item item : library.getItems()) {
+            System.out.println(item);
+        }
+    }
 }
