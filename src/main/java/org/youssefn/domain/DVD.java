c6143da0d6da33f59d7a714089e583c3baaad12c
youss <youssefna134@gmail.com>
2026-05-14 14:41:23 -0400

    Complete DVD and magazine class

--- src/main/java/org/youssefn/domain/DVD.java
+++ src/main/java/org/youssefn/domain/DVD.java
@@ -1,6 +1,19 @@
 package org.youssefn.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
 public class DVD extends Item {
     private String director;
     private int duration;
+
+    public DVD(String title, String director, int duration) {
+        super(title);
+        this.director = director;
+        this.duration = duration;
+    }
 }
--- src/main/java/org/youssefn/domain/Magazine.java
+++ src/main/java/org/youssefn/domain/Magazine.java
@@ -1,6 +1,19 @@
 package org.youssefn.domain;
 
-public class Magazine {
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class Magazine extends Item {
     private int issueNumber;
     private String publisher;
+
+    public Magazine(String title, int issueNumber, String publisher) {
+        super(title);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
 }
