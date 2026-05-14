f6e17fc91d1f165d277ada5e0ed0ae756025fb9c
youss <youssefna134@gmail.com>
2026-05-14 15:00:35 -0400

    User class with return/borrow methods

--- src/main/java/org/youssefn/domain/User.java
+++ src/main/java/org/youssefn/domain/User.java
@@ -1,10 +1,33 @@
 package org.youssefn.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@ToString
+@EqualsAndHashCode
 public abstract class User {
-    private int id;
+    private String id;
     private String name;
     private List<Item> borrowedItems;
 
+    private static int nextId = 1;
+
+    public User(String name) {
+        this.id = String.format("%04d", nextId++);
+        this.name = name;
+        this.borrowedItems = new ArrayList<>();
+    }
+
+    public void borrowItem(Item item) {
+        borrowedItems.add(item);
+    }
+
+    public void returnItem(Item item) {
+        borrowedItems.remove(item);
+    }
 }
